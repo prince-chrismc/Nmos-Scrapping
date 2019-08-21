01c8cf8485a5586d673896657cb53194f301f862
Chris McArthur <prince.chrismc@gmail.com>
2019-08-21 09:53:10 -0400

    now parses json from cli

--- nmos_registry/spiders/query_api_spider.py
+++ nmos_registry/spiders/query_api_spider.py
@@ -2,32 +2,37 @@ import scrapy
 from datetime import datetime
 import os
 import pathlib
+import json
+import sys
 
 # datetime object containing current date and time
 now = datetime.now()
 folder = now.strftime("%d-%m-%Y/%H-%M-%S/")
 pathlib.Path(folder).mkdir(parents=True, exist_ok=True)
 
+args = json.loads(sys.argv[-1])
+print(args)
+
 class QuerySpider(scrapy.Spider):
     name = "query"
 
     def start_requests(self):
         urls = [
-            'http://192.168.3.45/',
-            'http://192.168.3.45/x-nmos/query/v1.2/nodes/',
-            'http://192.168.3.45/x-nmos/query/v1.2/devices/',
-            'http://192.168.3.45/x-nmos/query/v1.2/senders/',
-            'http://192.168.3.45/x-nmos/query/v1.2/receivers/',
-            'http://192.168.3.45/x-nmos/query/v1.2/sources/',
-            'http://192.168.3.45/x-nmos/query/v1.2/flows/',
+            'nodes/',
+            'devices/',
+            'senders/',
+            'receivers/',
+            'sources/',
+            'flows/',
         ]
 
+        href = 'http://{0}:{1}/x-nmos/query/{2}/'.format(args["ip"], args["port"], args["api_ver"])
         for url in urls:
-            yield scrapy.http.JSONRequest(url=url, callback=self.parse)
+            yield scrapy.http.JSONRequest(url=href+url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'quotes-%s.json' % page
+        page = response.url.split("/")[-1]
+        filename = 'query-%s.json' % page
         with open(folder + filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)
