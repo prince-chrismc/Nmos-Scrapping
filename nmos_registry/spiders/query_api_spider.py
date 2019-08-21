adddd2a5ea5703fb8dbc63e2055f5b3590a3fc4c
Chris McArthur <prince.chrismc@gmail.com>
2019-08-21 11:58:27 -0400

    paging testing with order=update

--- nmos_registry/spiders/query_api_spider.py
+++ nmos_registry/spiders/query_api_spider.py
@@ -4,6 +4,7 @@ import os
 import pathlib
 import json
 import sys
+import requests
 
 # datetime object containing current date and time
 now = datetime.now()
@@ -30,8 +31,17 @@ class QuerySpider(scrapy.Spider):
             'flows',
         ]
 
+
+        r = requests.get('http://{0}:{1}/x-nmos/query/{2}/nodes?paging.order=update&paging.limit=10000000000000'.format(args["ip"], args["port"], args["api_ver"]))
+        try:
+            lim = r.headers['X-Paging-Limit']
+        except:
+            lim = -1
+
+        print('Paging Limit: {0}'.format(lim))
+
         for url in urls:
-            href = 'http://{0}:{1}/x-nmos/query/{2}/{3}?paging.order=update&paging.limit=10000'.format(args["ip"], args["port"], args["api_ver"], url)
+            href = 'http://{0}:{1}/x-nmos/query/{2}/{3}?paging.order=update&paging.limit={4}'.format(args["ip"], args["port"], args["api_ver"], url, lim)
             yield scrapy.http.JSONRequest(url=href, callback=self.parse)
 
     def parse(self, response):
@@ -40,6 +50,13 @@ class QuerySpider(scrapy.Spider):
         with open(folder + filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)
+
+        try:
+            since = response.headers['X-Paging-Since']
+            if since != b'0:0':
+                print('More then paging.limit {0}'.format(page))
+        except:
+             pass
 
     def closed(spider, reason):
         if reason == 'finished':
