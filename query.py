31c610f2038e2038aeb3fd8a4c62c26d583aaa0d
Chris McArthur <prince.chrismc@gmail.com>
2019-08-22 07:43:51 -0400

    new proto type for better paging support

---
+++ query.py
@@ -0,0 +1,68 @@
+from datetime import datetime
+import os
+import pathlib
+import json
+import sys
+import requests
+
+# datetime object containing current date and time
+now = datetime.now()
+folder = now.strftime("%d-%m-%Y/%H-%M-%S/")
+pathlib.Path(folder).mkdir(parents=True, exist_ok=True)
+
+args = json.loads(sys.argv[-1])
+print(args)
+
+class QueryScrapper():
+    name = "query"
+    endpoints = [
+        'nodes',
+        'devices',
+        'senders',
+        'receivers',
+        'sources',
+        'flows',
+    ]
+
+    def determine_paging_limit():
+        r = requests.get('http://{0}:{1}/x-nmos/query/{2}/nodes?paging.order=create&paging.limit=10000000000000'.format(args["ip"], args["port"], args["api_ver"]))
+        try:
+            lim = r.headers['X-Paging-Limit']
+        except:
+            lim = -1
+
+#        print('Paging Limit: {0}'.format(lim))
+        return lim
+
+    def start_requests(self):
+        lim = QueryScrapper.determine_paging_limit()
+        for url in self.endpoints:
+            href = 'http://{0}:{1}/x-nmos/query/{2}/{3}?paging.order=create&paging.limit={4}'.format(args["ip"], args["port"], args["api_ver"], url, lim)
+
+            index = 0
+            since = '0:0'
+            until = '0:1'
+
+            while True:
+                print('\n\nGET - ' + href + '&paging.since=' + until)
+                r = requests.get(href + '&paging.since=' + until)
+                filename = 'query-' + url
+                with open(folder + filename + '-{0}.json'.format(index), 'wb') as f:
+                    index += 1
+                    f.write(r.content)
+
+                print(r.headers)
+                since = r.headers['X-Paging-Since']
+                until = r.headers['X-Paging-Until']
+                print('Since: {0} // Until: {1}'.format(since, until))
+                if since == '0:0' or since == until:
+                    print('Obtained all ' + url)
+                    break
+
+                print('More then paging.limit of ' + url)
+
+    def close(self):
+        for filename in os.listdir(folder):
+            handle = open(folder + filename)
+            data = json.load(handle)
+            print('Resource in {0}: {1}'.format(filename, len(data)))
--- scrap-query.py
+++ scrap-query.py
@@ -1,8 +1,7 @@
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-from scrapy.utils.log import configure_logging
+from query import QueryScrapper
 
-process = CrawlerProcess(get_project_settings())
 
-process.crawl('query')
-process.start() # the script will block here until the crawling is finished
+scrapper = QueryScrapper()
+scrapper.start_requests()
+scrapper.close()
+
