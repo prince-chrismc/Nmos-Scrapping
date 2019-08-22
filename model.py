879adc8a2bb175a288b29a578113dcad2b7848da
Chris McArthur <prince.chrismc@gmail.com>
2019-08-22 10:31:55 -0400

    now displaying all resources

--- model.py
+++ model.py
@@ -32,7 +32,8 @@ def start_requests():
     data = json.loads(r.content)
     print('There are {0} nodes'.format(len(data)))
 
-    node_id = data[0]['id']
+#    node_id = data[0]['id']
+    node_id = '76ce6f35-bffd-27c3-a8d8-40a36ba0b212'
     href_associated_devices = 'http://{0}:{1}/x-nmos/query/{2}/{3}?paging.order=create&paging.limit={4}&node_id={5}'.format(args["ip"], args["port"], args["api_ver"], 'devices', lim, node_id)
     print('GET - ' + href_associated_devices)
     r = requests.get(href_associated_devices)
@@ -47,5 +48,24 @@ def start_requests():
         data = json.loads(r.content)
         for receiver in data:
             print('    - ' + receiver['id'])
+
+        href_sources = 'http://{0}:{1}/x-nmos/query/{2}/{3}?paging.order=create&paging.limit={4}&device_id={5}'.format(args["ip"], args["port"], args["api_ver"], 'sources', lim, device['id'])
+        print('\nGET - ' + href_sources)
+        r = requests.get(href_sources)
+        data = json.loads(r.content)
+        for source in data:
+            print('    - ' + source['id'])
+            href_flows = 'http://{0}:{1}/x-nmos/query/{2}/{3}?paging.order=create&paging.limit={4}&source_id={5}'.format(args["ip"], args["port"], args["api_ver"], 'flows', lim, source['id'])
+            print('\nGET - ' + href_flows)
+            r = requests.get(href_flows)
+            data = json.loads(r.content)
+            for flow in data:
+                print('        - ' + flow['id'])
+                href_flows = 'http://{0}:{1}/x-nmos/query/{2}/{3}?paging.order=create&paging.limit={4}&flow_id={5}'.format(args["ip"], args["port"], args["api_ver"], 'senders', lim, flow['id'])
+                print('\nGET - ' + href_flows)
+                r = requests.get(href_flows)
+                data = json.loads(r.content)
+                for sender in data:
+                    print('            - ' + sender['id'])
 
 start_requests()
